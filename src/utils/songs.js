fdae3930c9099b10d56586864664c70ce48546f7
Dhista Ardiansyah <56432757+gnotnek@users.noreply.github.com>
2023-12-19 13:05:20 +0700

    Add songs API implementation

---
+++ src/utils/songs.js
@@ -0,0 +1,19 @@
+const mapSongs = ({
+    id,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+    albumId,
+}) => ({
+    id,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+    albumId,
+});
+
+module.exports = { mapSongs };
